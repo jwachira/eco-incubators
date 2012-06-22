a9ab5d621f72363a891a60c9626cfd359a3b2080
chai2 <chaithumalla@gmail.com>
2012-06-21 21:06:46 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EcoIncubators::Application.config.secret_token = 'e6b6a0e1fce229b48cfcc1f5f288940913cffeffd4cc470e65496cfdaba289d4ad54461b41d4ea76e11b75979899740258dec13e1e38bbe5bd19a7afeb0466a3'
